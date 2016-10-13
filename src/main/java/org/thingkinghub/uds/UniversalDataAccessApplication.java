c67462c31beed7456caf9abd97c69d19641888b4
Wang <peng.wang9@hpe.com>
2016-10-13 16:43:00 +0800

    rename spring boot main class

--- src/main/java/org/thingkinghub/uds/UniversalDataAccessApplication.java
+++ src/main/java/org/thingkinghub/uds/UniversalDataAccessApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UniversaryDataAccessApplication {
+public class UniversalDataAccessApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UniversaryDataAccessApplication.class, args);
+		SpringApplication.run(UniversalDataAccessApplication.class, args);
 	}
 }
--- src/test/java/org/thingkinghub/uds/UniversalDataAccessApplicationTests.java
+++ src/test/java/org/thingkinghub/uds/UniversalDataAccessApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class UniversaryDataAccessApplicationTests {
+public class UniversalDataAccessApplicationTests {
 
 	@Test
 	public void contextLoads() {
